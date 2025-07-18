dee82f9a93bcce7b655c22a29f501668c1e07cf2
AshimChoudhary <choudharyashim99@gmail.com>
2025-07-18 14:49:02 +0530

    A

--- frontend/src/component/ChatContainer.jsx
+++ frontend/src/component/ChatContainer.jsx
@@ -47,7 +47,7 @@ const ChatContainer = () => {
             </div>
             <div className="chat-header mb-1">
               <time className="text-xs opacity-50 ml-1 ">
-                {formatMessageTime(message.createdAt)}s
+                {formatMessageTime(message.createdAt)}
               </time>
             </div>
             <div className="chat-bubble flex flex-col">
